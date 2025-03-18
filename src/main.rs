c7467bf67ef1235623637ddbe2334a42ea7f2e02
Arne Nilsen <arne.nilsen@skatteetaten.no>
2025-03-18 20:59:57 +0100

    Cleaning

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
-mod kafka_consumer;
-mod greetings;
-mod settings;
-mod db;
 mod blob_writer;
+mod db;
+mod greetings;
+mod kafka_consumer;
+mod settings;
 
-use futures_util::join;
-use opentelemetry::{global};
+use crate::greetings::GreetingRepositoryImpl;
+use crate::kafka_consumer::ConsumeTopics;
+use crate::settings::Settings;
+use opentelemetry::global;
 use opentelemetry::trace::TracerProvider;
-use opentelemetry_appender_tracing::layer::OpenTelemetryTracingBridge;
-use opentelemetry_sdk::logs::LoggerProvider;
-use opentelemetry_sdk::propagation::TraceContextPropagator;
-use sqlx::Pool;
-use sqlx::postgres::PgPoolOptions;
-use tracing_subscriber::EnvFilter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
-use crate::greetings::{GreetingRepositoryImpl};
-use crate::kafka_consumer::{ConsumeTopics};
-use crate::settings::Settings;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let app_config = Settings::new();
 
-    greeting_otel::init_otel(&app_config.otel_collector.oltp_endpoint, "greeting_processor",&app_config.kube.my_pod_name).await;
+    greeting_otel::init_otel(
+        &app_config.otel_collector.oltp_endpoint,
+        "greeting_processor",
+        &app_config.kube.my_pod_name,
+    )
+    .await;
 
-    let pool = Box::new(db::init_db(app_config.db.database_url.clone()).await.expect("Expected db pool"));
-    let repo = Box::new(GreetingRepositoryImpl::new(pool.clone()).await.expect("failed"));
-    let mut consumer = kafka_consumer::KafkaConsumer::new(app_config, repo).await.expect("Failed to create kafka consumer");
+    let pool = Box::new(
+        db::init_db(app_config.db.database_url.clone())
+            .await
+            .expect("Expected db pool"),
+    );
+    let repo = Box::new(
+        GreetingRepositoryImpl::new(pool.clone())
+            .await
+            .expect("failed"),
+    );
+    let mut consumer = kafka_consumer::KafkaConsumer::new(app_config, repo)
+        .await
+        .expect("Failed to create kafka consumer");
 
-    consumer.consume_and_store().await.expect("Error in kafka consumer");
+    consumer
+        .consume_and_store()
+        .await
+        .expect("Error in kafka consumer");
 
     global::shutdown_tracer_provider();
     Ok(())
 }
-
-
